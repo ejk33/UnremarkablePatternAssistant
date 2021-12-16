c9de2a9ba13279c717871a0f1a908638a1609327
ejk33 <elijahk33@gmail.com>
2021-12-16 17:32:58 +0000

    Add dropzone

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
 // @flow
 
+import FileInput from "./FileInput.react";
+
 import React from "react";
+import { useCallback } from "react";
+
+const styles = {
+  container: {
+    padding: '16px'
+  }
+}
 
 function Header(): React$MixedElement {
   return <h1>UNREMARKABLE PATTERN ASSISTANT</h1>;
 }
 
 function App(): React$MixedElement {
-  return <Header />;
+  const onFilesChange = useCallback(file => {
+    console.info('<><><> selected file is', file);
+  }, []);
+
+  return (
+    <div style={styles.container}>
+      <Header />
+      <FileInput onFilesChange={onFilesChange} />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/FileInput.react.js
@@ -0,0 +1,41 @@
+// @flow
+
+import React from "react";
+import { useEffect } from "react";
+
+import {useDropzone} from 'react-dropzone';
+
+const styles = {
+    dropzone: {
+        padding: '16px',
+        borderStyle: 'solid',
+        borderColor: '#c5c5c5'
+    },
+    message: {
+        padding: '16px',
+    }
+}
+
+type Props = {
+    onFilesChange: (file: File) => void;    
+};
+
+export default function FileInput({onFilesChange}: Props): React$MixedElement {
+  const {acceptedFiles, getRootProps, getInputProps} = useDropzone({
+      multiple: false
+  });
+  
+  useEffect(() => {
+      if (acceptedFiles.length == 0) {
+          return;
+      }
+      onFilesChange(acceptedFiles[0]);
+  }, [acceptedFiles]);
+
+  return (
+      <div style={styles.dropzone} {...getRootProps({})}>
+        <input {...getInputProps()} />
+        <p>Drop or choose a difficulty.dat file, such as ExpertPlusStandard.dat</p>
+      </div>
+  );
+}
