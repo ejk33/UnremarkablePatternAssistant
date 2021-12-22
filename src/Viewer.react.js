dbd0355c57c5b86573282bebf602fdc73fc5b403
ejk33 <elijahk33@gmail.com>
2021-12-22 18:52:16 +0000

    Test offlineaudiocontext

--- src/Viewer.react.js
+++ src/Viewer.react.js
@@ -3,7 +3,7 @@
 import type { BeatMap } from "./MapArchive";
 
 import React from 'react';
-import { useCallback, useEffect, useState } from "react";
+import { lazy, useCallback, useEffect, useState } from "react";
 import * as WaveSurfer from 'wavesurfer.js';
 
 export const VIEWER_PX_PER_SEC = 250;
@@ -25,12 +25,9 @@ const styles = {
     }
 }
 
-type FilterType = 'main' | 'low' | 'mid' | 'high';
+type FilterType = 'low' | 'mid' | 'high';
 
 function createFilters(wavesurfer: any, type: FilterType): Array<any> | null {
-    if (type === 'main') {
-        return null;
-    }
     const ac: BaseAudioContext = wavesurfer.backend.ac;
     if (type === 'low') {
         const filter = ac.createBiquadFilter();
@@ -42,7 +39,7 @@ function createFilters(wavesurfer: any, type: FilterType): Array<any> | null {
     return null;
 }
 
-function createWaveSurfer(domSelector: string, beatMap: BeatMap, type: FilterType): any {
+function createWaveSurfer(domSelector: string, beatMap: BeatMap): any {
     const wavesurfer = WaveSurfer.create({
         container: domSelector,
         scrollParent: true,
@@ -56,13 +53,107 @@ function createWaveSurfer(domSelector: string, beatMap: BeatMap, type: FilterTyp
         waveColor: '#555',
         cursorWidth: 0
     });
-    const filters = createFilters(wavesurfer, type);
-    if (filters != null) {
-        wavesurfer.backend.setFilter(filters[0]);
-    }
+    // const filters = createFilters(wavesurfer, type);
+    // if (filters != null) {
+    //     wavesurfer.backend.setFilter(filters[0]);
+    // }
     wavesurfer.loadBlob(beatMap.songBlobData);
     wavesurfer.zoom(VIEWER_PX_PER_SEC);
     return wavesurfer;
+}
+
+// async function createFilteredWaveSurfer(mainWaveSurfer: any, beatMap: BeatMap): Promise<void> {
+//     let tmpDiv = null;
+//     try {
+//         await new Promise((resolve) => {
+//             console.info('Creating filtered wave surfer');
+//             tmpDiv = document.createElement('div');
+//             const tmpId = 'tmpwavesurfer';
+//             tmpDiv.id = tmpId;
+//             document.body?.appendChild(tmpDiv);
+
+//             const tmpWave = WaveSurfer.create({
+//                 container: `#${tmpId}`,
+//             });
+//             const ac: BaseAudioContext = tmpWave.backend.ac;
+//             const filter = ac.createBiquadFilter();
+//             filter.frequency.setValueAtTime(250, 0);
+//             filter.type = 'lowpass';
+//             tmpWave.backend.setFilter(filter);
+//             tmpWave.loadBlob(beatMap.songBlobData);
+//             tmpWave.on('ready', () => {
+//                 console.info('tmp wave ready');
+//                 // $FlowFixMe
+//                 const offlineCtx = new OfflineAudioContext({
+//                     numberOfChannels: 2,
+//                     length: Math.ceil(44100 * mainWaveSurfer.getDuration()),
+//                     sampleRate: 44100,
+//                 });
+//                 console.info('offline ctx made');
+//                 const source = offlineCtx.createBufferSource();
+
+//                 ac.createBuffer()
+//                 ac.decodeAudioData(buffer => {
+                    
+//                 })
+
+//                 source.connect(offlineCtx.destination);
+//                 source.start();
+//                 console.info('rendering starting');
+//                 offlineCtx.startRendering().then(renderedBuffer => {
+//                     console.info('Rendering completed');
+//                     const wavesurfer = WaveSurfer.create({
+//                         container: '#waveform-low',
+//                         scrollParent: true,
+//                         autoCenter: false,
+//                         partialRender: true,
+//                         removeMediaElementOnDestroy: true,
+//                         hideCursor: true,
+//                         interact: false,
+//                         hideScrollbar: true,
+//                         progressColor: '#555',
+//                         waveColor: '#555',
+//                         cursorWidth: 0
+//                     });
+//                     console.info('Loading from rendered buffer');
+//                     wavesurfer.loadBlob(renderedBuffer);
+//                     wavesurfer.on('ready', () => {
+//                         console.info('modified surfer ready');
+//                     });
+//                     wavesurfer.on('error', err => {
+//                         console.info(err);
+//                     });
+//                 }).catch(err => {
+//                     console.info('error in startRendering', err);
+//                 })
+
+//                 const pcm = tmpWave.exportPCM();
+//                 console.info(pcm);
+//                 // resolve();
+//             });
+//         });
+//     } finally {
+//         if (tmpDiv != null) {
+//             // document.body?.removeChild(tmpDiv);
+//         }
+//     }
+// }
+
+async function createFilteredWaveSurfer(mainPlayer: any, beatMap: BeatMap): Promise<void> {
+    let audioCtx = new AudioContext();
+    // $FlowFixMe
+    let offlineCtx = new OfflineAudioContext(2, Math.ceil(44100*mainPlayer.getDuration()), 44100);
+    let source = offlineCtx.createBufferSource();
+    // $FlowFixMe
+    audioCtx.decodeAudioData(beatMap.songBlobData, function(buffer) {
+        source.buffer = buffer;
+        source.connect(offlineCtx.destination);
+        source.start();
+        offlineCtx.startRendering().then(function(renderedBuffer) {
+            console.info('Rendering done');
+        });
+        
+    });
 }
 
 export function Viewer({beatMap}: Props): React$MixedElement | null {
@@ -70,8 +161,9 @@ export function Viewer({beatMap}: Props): React$MixedElement | null {
 
     useEffect(() => {
         setMainPlayer(() => {
-            createWaveSurfer('#waveform', beatMap, 'main');
-            createWaveSurfer('#waveform-low', beatMap, 'low');
+            const mainPlayer = createWaveSurfer('#waveform', beatMap);
+            createFilteredWaveSurfer(mainPlayer, beatMap);
+            return mainPlayer;
         });
     }, [beatMap]); 
 
